5ee037bddc6f6c24a8e92151f1579ee5d355d699
Rafael Orman <Kronos9247@users.noreply.github.com>
2019-05-07 21:49:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+var util = require("util");
 
 function mysql_createpool(hostname, username, password, database=undefined, port=undefined, poolSize=100) {
     return mysql.createPool({
@@ -9,29 +10,41 @@ function mysql_createpool(hostname, username, password, database=undefined, port
         port: port,
 
         connectionLimit: poolSize
-    })
+    });
 }
 module.exports.createPool = mysql_createpool;
 
-async function mysql_getconnection(con) {
-    let connection = await new Promise((resolve, reject) => {
-        con.getConnection(function(err, con){
-            if (err) reject(err);
+async function mysql_getconnection(pool) {
+    var getConnection = pool.getConnection;
+    let connection = await util.promisify(getConnection.bind(pool))();
+    // let connection = await new Promise((resolve, reject) => {
+    //     pool.getConnection(function(err, con){
+    //         if (err) reject(err);
     
-            resolve(con);
-        });
-    });
+    //         resolve(con);
+    //     });
+    // });
 
     return connection;
 }
 function mysql_getconnection_autoclose(pool, callback, next=undefined) {
-    mysql_getconnection(pool).then((con) => {
-        callback(con);
+    let promise = mysql_getconnection(pool).then((con) => {
+        new Promise(function(resolve) {
+            callback(con);
 
-        con.release();
-    }).catch((err) => {
-        if (next) next(err);
-        else throw err;
+            resolve();
+        }).then(() => con.release()).catch(function(err) {
+            con.release();
+
+            if(typeof next === "function") next(err);
+            else throw err;
+        });
     });
+
+
+    if(typeof next === "function") {
+        promise.catch(next);
+    }
+    else promise.catch((err) => { throw err });
 }
 module.exports.getConnection = mysql_getconnection_autoclose;
